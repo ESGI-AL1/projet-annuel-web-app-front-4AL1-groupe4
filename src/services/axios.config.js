3edb669e62a23c5d3bd6fa8ddd921149e38ace77
HubertZodjihoue <joycetreavis@gmail.com>
2024-07-10 14:41:21 +0200

    add update url

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const URL =
-    // eslint-disable-next-line no-undef
-    process.env.NODE_ENV === "production"
-        ? import.meta.env.VITE_BACKEND_URL
-        : "http://localhost:8000/api";
-
+    window.location.hostname === "localhost"
+        ? "http://localhost:8000/api"
+        : "http://ec2-13-60-35-3.eu-north-1.compute.amazonaws.com:8000/api";
 
 export const baseUrl = axios.create({
     baseURL: `${URL}`,
