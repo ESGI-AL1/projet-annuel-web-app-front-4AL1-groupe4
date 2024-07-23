b3ee292a81abc91bd66beb8ca88ad12f90d20b0e
HubertZodjihoue <joycetreavis@gmail.com>
2024-07-23 09:56:28 +0200

    update all functionnalities

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const URL =
     window.location.hostname === "localhost"
-        ? "http://localhost:8000/api"
-        : "http://ec2-13-60-35-3.eu-north-1.compute.amazonaws.com:8000/api";
+        ? "http://ec2-13-60-35-3.eu-north-1.compute.amazonaws.com:8000/api" : '';
 
 export const baseUrl = axios.create({
     baseURL: `${URL}`,
