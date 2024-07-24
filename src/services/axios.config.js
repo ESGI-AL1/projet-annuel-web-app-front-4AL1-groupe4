15514969d11b43b0e1023f2c9cbeeb5ff00ea465
HubertZodjihoue <joycetreavis@gmail.com>
2024-07-24 19:32:59 +0200

    url without https

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const URL =
-    window.location.hostname === "localhost"
-        ? "http://ec2-13-53-40-36.eu-north-1.compute.amazonaws.com:8000/api" : 'https://ec2-13-53-40-36.eu-north-1.compute.amazonaws.com:8000/api';
-
+const URL = "http://ec2-13-53-40-36.eu-north-1.compute.amazonaws.com:8000/api"
 export const baseUrl = axios.create({
     baseURL: `${URL}`,
 });
